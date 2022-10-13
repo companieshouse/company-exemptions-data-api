d7e84933193ce9351cf7003071d275bec8d612d4
sthompsonCH <sthompson@companieshouse.gov.uk>
2022-10-13 16:44:29 +0100

    Add boilerplate code
    * Add Makefile
    * Add pom.xml
    * Add spring application file
    * Update gitignore
    * Add start script
    * Add tiltfile

---
+++ src/main/java/uk/gov/companieshouse/exemptions/ExemptionsApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.exemptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/companieshouse/exemptions/ExemptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.exemptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
