03d8c23061d28a163969ba1bf0b1e3caf46d1ab5
cgregoryCH <cgregory@companieshouse.gov.uk>
2022-11-03 17:14:13 +0000

    Remove serialisation classes and object mapper bean

--- src/main/java/uk/gov/companieshouse/exemptions/Config.java
+++ src/main/java/uk/gov/companieshouse/exemptions/Config.java
@@ -1,15 +1,11 @@
 package uk.gov.companieshouse.exemptions;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import java.time.OffsetDateTime;
 import java.util.function.Supplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
-
-import java.time.LocalDate;
 
 import static uk.gov.companieshouse.exemptions.ExemptionsApplication.NAMESPACE;
 
@@ -24,16 +20,6 @@ public class Config {
     @Bean
     public Supplier<String> offsetDateTimeGenerator() {
         return () -> String.valueOf(OffsetDateTime.now());
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(LocalDate.class, new LocalDateSerializer());
-        module.addDeserializer(LocalDate.class, new LocalDateDeSerializer());
-        mapper.registerModule(module);
-        return mapper;
     }
 }
 
