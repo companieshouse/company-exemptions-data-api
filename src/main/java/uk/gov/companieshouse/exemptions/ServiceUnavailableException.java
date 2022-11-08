0028167f57a7982136fb664de9b9f3400ed681ba
cgregoryCH <cgregory@companieshouse.gov.uk>
2022-11-08 11:45:34 +0000

    Add mongoDbConfig to remove _class property from mongo

--- src/main/java/uk/gov/companieshouse/exemptions/ServiceUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.companieshouse.exemptions;
-
-public class ServiceUnavailableException extends RuntimeException {
-    public ServiceUnavailableException(String message) {
-        super(message);
-    }
-}
