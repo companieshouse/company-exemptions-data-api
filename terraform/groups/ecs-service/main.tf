6f12ceffa7eef6ad75be734a2add895f9f29d949
chillier-ch <chillier@companieshouse.gov.uk>
2024-03-19 16:53:04 +0000

    Remove duplicate module

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -89,12 +89,3 @@ module "ecs-service" {
   eric_environment_filename = local.eric_environment_filename
   eric_secrets              = local.eric_secrets
 }
-
-module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.253"
-
-  name_prefix = "${local.service_name}-${var.environment}"
-  environment = var.environment
-  kms_key_id  = data.aws_kms_key.kms_key.id
-  secrets     = nonsensitive(local.service_secrets)
-}
