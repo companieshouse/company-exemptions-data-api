fefcea76a588fdc6bc832ed3a8d583f2b57ec378
Martin-Warnett <mwarnett@companieshouse.gov.uk>
2026-05-29 14:09:22 +0100

    asm-2324: Removed unused import.

--- src/test/java/uk/gov/companieshouse/exemptions/exception/ExemptionsReadExceptionTest.java
+++ src/test/java/uk/gov/companieshouse/exemptions/exception/ExemptionsReadExceptionTest.java
@@ -1,8 +1,8 @@
 package uk.gov.companieshouse.exemptions.exception;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 
 class ExemptionsReadExceptionTest {
     @Test
