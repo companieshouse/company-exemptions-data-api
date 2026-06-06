a2c6f0cef6313cedc5c9f7e8e4c0dc115e9a96ec
Martin-Warnett <mwarnett@companieshouse.gov.uk>
2026-06-06 10:53:56 +0100

    asm-2324: Renamed application property.
    Health check test was failing - management.health.mongo.enabled is now management.health.mongodb.enabled.
    
    Removed TestContainers attempted fix from test.

--- src/test/java/uk/gov/companieshouse/exemptions/ExemptionsApplicationTest.java
+++ src/test/java/uk/gov/companieshouse/exemptions/ExemptionsApplicationTest.java
@@ -11,21 +11,12 @@ import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.mongodb.MongoDBContainer;
 
-//@Testcontainers
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ExemptionsApplicationTest {
-//
-//	@Container
-//	@ServiceConnection
-//	static final MongoDBContainer MONGO = new MongoDBContainer("mongo:8.0");
 
 	@Autowired
 	private MockMvc mockMvc;
