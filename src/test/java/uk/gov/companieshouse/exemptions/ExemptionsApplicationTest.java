33f7ed6492f5c6cef23ec8b597100e1498f010f1
Martin-Warnett <mwarnett@companieshouse.gov.uk>
2026-06-22 15:24:29 +0100

    ASM-2324: Fixed failing test.

--- src/test/java/uk/gov/companieshouse/exemptions/ExemptionsApplicationTest.java
+++ src/test/java/uk/gov/companieshouse/exemptions/ExemptionsApplicationTest.java
@@ -1,30 +1,29 @@
 package uk.gov.companieshouse.exemptions;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ExemptionsApplicationTest {
+	@LocalServerPort
+	int port;
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
 	void shouldStartApplication() {
-		Executable executable = () -> ExemptionsApplication.main(new String[0]);
-		assertDoesNotThrow(executable);
 	}
 
 	@Test
